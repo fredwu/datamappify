400c6ea46ba5e1c53c5424993ee370b14b64c321
Fred Wu <fred@envato.com>
2010-07-26 12:10:22 +1000

    Use reflection to get the HABTM associations data.

--- lib/datamappify/associations.rb
+++ lib/datamappify/associations.rb
@@ -11,26 +11,8 @@ module Datamappify
       end
       
       def has_and_belongs_to_many(association_id, options = {}, &extension)
-        join_table_name         = options.include?(:join_table) ?
-                                  options[:join_table] : join_table_name(table_name, association_id.to_s.classify.constantize.table_name)
-        foreign_key             = options.include?(:foreign_key) ? options[:foreign_key] : self.to_s.foreign_key
-        association_foreign_key = options.include?(:association_foreign_key) ?
-                                  options[:association_foreign_key] : association_id.to_s.classify.to_s.foreign_key
-        
-        Associations.join_tables[join_table_name] = [foreign_key, association_foreign_key]
-      end
-      
-      private
-      
-      # taken from ActiveRecord::Associations::ClassMethods
-      def join_table_name(first_table_name, second_table_name)
-        if first_table_name < second_table_name
-          join_table = "#{first_table_name}_#{second_table_name}"
-        else
-          join_table = "#{second_table_name}_#{first_table_name}"
-        end
-
-        table_name_prefix + join_table + table_name_suffix
+        reflection = self.send :create_has_and_belongs_to_many_reflection, association_id, {}
+        Associations.join_tables[reflection.options[:join_table]] = [reflection.association_foreign_key, reflection.primary_key_name]
       end
     end
   end

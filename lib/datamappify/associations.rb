f6a0491cd2325295b75286b469287c0f50cdefb5
Fred Wu <fred@envato.com>
2010-07-27 10:09:32 +1000

    Forgot to call super on the association methods.

--- lib/datamappify/associations.rb
+++ lib/datamappify/associations.rb
@@ -6,11 +6,13 @@ module Datamappify
     
     module ClassMethods
       def belongs_to(association_id, options = {})
+        super
         foreign_key = options.include?(:foreign_key) ? options[:foreign_key] : association_id.to_s.foreign_key
         property(foreign_key.to_sym, :integer)
       end
       
       def has_and_belongs_to_many(association_id, options = {}, &extension)
+        super
         reflection = self.send :create_has_and_belongs_to_many_reflection, association_id, options
         Associations.join_tables[reflection.options[:join_table]] = [reflection.association_foreign_key, reflection.primary_key_name]
       end

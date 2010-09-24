fc85b8c03772da0fff1d3f1596855ac593b0f9c3
Fred Wu <fred@envato.com>
2010-09-24 14:42:43 +1000

    Makes sure Railtie is present if we were going to inherit it.

--- lib/datamappify/railtie.rb
+++ lib/datamappify/railtie.rb
@@ -1,8 +1,10 @@
-module Datamappify
-  class Railtie < Rails::Railtie
-    rake_tasks do
-      load File.expand_path("../../tasks/datamappify.rake", __FILE__)
-      load File.expand_path("../../../vendor/auto_migrations/lib/tasks/auto_migrations_tasks.rake", __FILE__)
+if defined?(Rails::Railtie)
+  module Datamappify
+    class Railtie < Rails::Railtie
+      rake_tasks do
+        load File.expand_path("../../tasks/datamappify.rake", __FILE__)
+        load File.expand_path("../../../vendor/auto_migrations/lib/tasks/auto_migrations_tasks.rake", __FILE__)
+      end
     end
   end
 end

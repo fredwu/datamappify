ae1a055b4ff4b35f87563b3313aed40523fb341a
Fred Wu <ifredwu@gmail.com>
2013-04-12 00:48:35 +1000

    Minor tweaks to QueryMethod::Destroy#result

--- lib/datamappify/repository/query_method/destroy.rb
+++ lib/datamappify/repository/query_method/destroy.rb
@@ -10,11 +10,13 @@ module Datamappify
         end
 
         def result
-          ids = Array.wrap(@id_or_ids_or_entity_or_entities).map do |id_or_entity|
+          entities = Array.wrap(@id_or_ids_or_entity_or_entities).map do |id_or_entity|
             @mapper.default_provider.build_criteria(
               :Destroy, @mapper.default_source_class, extract_entity_id(id_or_entity)
             )
           end
+
+          @id_or_ids_or_entity_or_entities.is_a?(Array) ? entities : entities[0]
         end
       end
     end

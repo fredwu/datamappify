219bcdffa4c01b2dcefb37a5a87656b6fb0df662
Fred Wu <ifredwu@gmail.com>
2013-05-02 22:36:37 +1000

    Minor renaming

--- lib/datamappify/repository/unit_of_work/transaction.rb
+++ lib/datamappify/repository/unit_of_work/transaction.rb
@@ -2,14 +2,14 @@ module Datamappify
   module Repository
     module UnitOfWork
       class Transaction
-        # @param mapper (see QueryMethod::Method#initialize)
+        # @param data_mapper (see QueryMethod::Method#initialize)
         #
         # @yield
         #   queries to be performed in the transaction
         #
         # @return [void]
-        def initialize(mapper, &block)
-          mapper.default_provider.build_criteria(:Transaction, mapper.default_source_class, &block)
+        def initialize(data_mapper, &block)
+          data_mapper.default_provider.build_criteria(:Transaction, data_mapper.default_source_class, &block)
         end
       end
     end

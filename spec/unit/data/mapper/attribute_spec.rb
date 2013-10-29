5cc469d7d1c13eafbd3b176d626da0069b92516b
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2013-10-29 15:30:39 +1100

    Attribute spec more clearly uses different data for different expected outputs.

--- spec/unit/data/mapper/attribute_spec.rb
+++ spec/unit/data/mapper/attribute_spec.rb
@@ -8,14 +8,14 @@ describe Datamappify::Data::Mapper::Attribute do
       let(:record_class) { data_provider_class.find_or_build_record_class('Fridge::Freezer') }
 
       subject { described_class.new(:door,
-                                    to: 'Fridge::Freezer#door',
+                                    to: 'Fridge::Freezer#door_handle',
                                     provider: data_provider,
                                     primary_source_class: record_class ) }
 
       its(:provider_name)         { should == data_provider }
       its(:key)                   { should == :door }
       its(:name)                  { should == 'door' }
-      its(:source_attribute_name) { should == 'door' }
+      its(:source_attribute_name) { should == 'door_handle' }
       its(:source_class_name)     { should == 'Fridge::Freezer' }
       its(:source_table)          { should == :fridge_freezers }
     end
